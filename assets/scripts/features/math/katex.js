169b498855c8a024c2a3c1f81372ce8671699eef
Rich Baird <rich@rbaird.me>
2023-07-25 19:42:09 -0600

    update katex script to automatically replace backslashes and work with id anchors

--- assets/scripts/features/math/katex.js
+++ assets/scripts/features/math/katex.js
@@ -2,15 +2,29 @@ import renderMathInElement from 'katex/contrib/auto-render'
 import * as params from '@params'
 
 const defaultOptions = {
+  trust: (context) => ['\\htmlId', '\\href'].includes(context.command),
   delimiters: [
     { left: '$$', right: '$$', display: true },
     { left: '\\[', right: '\\]', display: true },
     { left: '$', right: '$', display: false },
-    { left: '\\(', right: '\\)', display: false }
-  ]
+    { left: '\\(', right: '\\)', display: true }
+  ],
+  throwOnError: true,
+}
+
+/**
+ * Replace single slash with double slash
+ * @param {*} element 
+ * @returns 
+ */
+const replaceDoubleSlash = (element) => {
+  if (!element) return
+  element.innerHTML = element.innerHTML.replace(/\\(\s+)/g, '\\\\$1')
+  console.log(element.innerHTML)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  replaceDoubleSlash(document.body.querySelector('body .post-content'))
   renderMathInElement(
     document.body,
     {
